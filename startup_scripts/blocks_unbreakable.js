e497a120500cbaca198c5dcb8101eb703070deaa
whakened <whakened@gmail.com>
2024-09-20 17:22:48 -0400

    Reduce bloating of blocks_unbreakable.js

--- startup_scripts/blocks_unbreakable.js
+++ startup_scripts/blocks_unbreakable.js
@@ -1,91 +1,38 @@
 // priority: 0
-
+const samples = [
+	'amethyst',
+	'bismuthinite',
+	'bituminous_coal',
+	'borax',
+	'cassiterite',
+	'cinnabar',
+	'copper',
+	'cryolite',
+	'diamond_sample',
+	'emerald_sample',
+	'garnierite',
+	'golds',		// This is correct, it is defined as such in the mod unfortunately
+	'graphite',
+	'gypsum',
+	'halite',
+	'hematite',
+	'kaolinite',
+	'lapislazuli',
+	'lignite',
+	'limonite',
+	'magnetite',
+	'malachite',
+	'saltpeter',
+	'silver',
+	'sphalerite',
+	'sulfur',
+	'tetrahedrite',
+	'vulcanic_sulfur'
+]
 onEvent('block.modification', event => {
-	event.modify('hardrock_samples:amethyst', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:bismuthinite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:bituminous_coal', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:borax', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:cassiterite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:cinnabar', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:copper', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:cryolite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:diamond_sample', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:emerald_sample', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:garnierite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:golds', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:graphite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:gypsum', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:halite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:hematite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:koalinite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:lapislazuli', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:lignite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:limonite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:magnetite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:malachite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:saltpeter', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:silver', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:sphalerite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:sulfur', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:sylvite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:tetrahedrite', block => {
-	  block.destroySpeed = -1
-	})
-  event.modify('hardrock_samples:vulcanic_sulfur', block => {
-	  block.destroySpeed = -1
-	})
+	samples.forEach((name) => {
+		event.modify('hardrock_samples:' + name, block => {
+			block.destroySpeed = 0
+		})
+	});
 })
