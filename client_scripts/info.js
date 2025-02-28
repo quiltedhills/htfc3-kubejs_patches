56f939d550bb7e1384f32825b7adb3dc1344bdff
air <quiltedhills@protonmail.com>
2025-02-28 13:37:12 +0500

    move respirator tooltips to info.js

--- client_scripts/info.js
+++ client_scripts/info.js
@@ -415,4 +415,26 @@ onEvent('item.tooltip', tooltip => {
 		}
 	})
 	tooltip.add('waystones:waystone', ["Hard item to craft, don't lose it", 'will provide you with a way to teleport'])
+	
+    tooltip.addAdvanced('adpother:diamond_respirator', (item, advanced, text) => {
+		if (!tooltip.shift) {
+		  text.add(1, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
+		} else {
+			text.add(1, Text.gold('Prevents eating and some right-click interactions' ))			
+		}
+    })
+	tooltip.addAdvanced('adpother:gold_respirator', (item, advanced, text) => {
+		if (!tooltip.shift) {
+		  text.add(1, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
+		} else {
+			text.add(1, Text.gold('Prevents eating and some right-click interactions' ))			
+		}
+    })
+    tooltip.addAdvanced('adpother:iron_respirator', (item, advanced, text) => {
+		if (!tooltip.shift) {
+		  text.add(1, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
+		} else {
+			text.add(1, Text.gold('Prevents eating and some right-click interactions' ))			
+		}
+    })
 })
