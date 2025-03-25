e9cf29b2b5d4e504d374a6cf5a8e3365cda4898f
SilverSBH <nbrisbine@gmail.com>
2025-03-24 19:46:34 -0500

    LargeMeatsJeiInfo
    The description is VERY lengthy as i am not blessed with the gift of brevity. Feel free to adjust lmao

--- client_scripts/jei_info.js
+++ client_scripts/jei_info.js
@@ -3,4 +3,11 @@ onEvent('jei.information', event => {
         'Made by right clicking a stripped log with an axe in your main hand and a hammer in your offhand!',
         'Stone tools can be used for this.'
     ])
+	event.add('#tfc:foods/meats/large', [
+        'Preserving large cuts of meat is possible, but can be wasteful if done incorrectly!',
+		'If chopping a preserved large meat in a crafting table, it costs more salt, brine, and vinegar than chopping first and preserving later. Additionally, the decay is carried over.',
+		'However, if you use a cutting board to chop, you get double the meat but lose the preservation entirely.',
+		'The key to this is that a cutting board removes preservation AND decay!',
+		'This means if you have a near-rotten large meat, you can chop it on a cutting board and receive fresh meat!'
+    ])
 })
