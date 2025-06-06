b66ccc4aa84eb07e55bec31be53306bed2228a32
SilverSBH <nbrisbine@gmail.com>
2025-06-06 14:16:03 -0500

    removeSolarDrierMatRecipe
    To help prevent players getting confused

--- server_scripts/remove_id.js
+++ server_scripts/remove_id.js
@@ -398,6 +398,7 @@ onEvent('recipes', event => {
 		{id: 'immersiveposts:has_uranium_rod'},
 		{id: 'firmalife:pot/beet_sugar'},
 		{id: 'firmalife:crafting/empty_jar'},
+    {id: 'firmalife:crafting/solar_drier_from_mat'},
 		{id: 'mekanism:infusion_conversion/carbon/from_enriched'},
 		{id: 'mekanism:infusion_conversion/carbon/from_charcoal_block'},
 		{id: 'mekanism:infusion_conversion/carbon/from_charcoal'},
